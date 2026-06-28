63b28e53bf700365db942eb6b927fc0c2157c08e
Tien Luong Van <luongvantien27052006@gmail.com>
2026-06-29 06:56:12 +0700

    Thêm ảnh

--- next.config.mjs
+++ next.config.mjs
@@ -7,3 +7,16 @@ const nextConfig = {
 };
 
 export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
